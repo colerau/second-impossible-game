01ecc852e2f1b83c849cb190e42b8c574f284d6f
Cole Rau <colevrau@gmail.com>
2020-09-10 21:26:44 -0700

    user can click on signUp button and js recognizes event

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,24 @@
 // console.log("yeet");
 
-const BACKEND_URL = "http://localhost:3000";
-fetch(`${BACKEND_URL}/users`)
-    .then(resp => resp.json())
-    .then(parsedResp => console.log(parsedResp));
+// const BACKEND_URL = "http://localhost:3000";
+// fetch(`${BACKEND_URL}/users`)
+//     .then(resp => resp.json())
+//     .then(parsedResp => console.log(parsedResp));
 
+document.addEventListener("DOMContentLoaded", () => {
+    start();
+})
+
+function start() {
+    const signUpButton = document.getElementById("signUpButton");
+
+    signUpButton.addEventListener("click", (e) => {
+        signUp(e);
+    })
+    
+}
+
+function signUp(e) {
+    e.preventDefault();
+    console.log("user hit sign up button");
+}
