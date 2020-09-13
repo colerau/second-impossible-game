bee617cfe9a36e546a816f0af34430b4f16ed763
Cole Rau <colevrau@gmail.com>
2020-09-13 13:05:49 -0700

    yet again, the code still works after moving stuff around

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const signUpButton = document.getElementById("signUpButton");
+
+    const logInButton = document.querySelector("#logInButton");
+    
+    signUpButton.addEventListener("click", (e) => {
+        signUp(e);
+    })
+    
+    logInButton.addEventListener("click", (e) => {
+        logIn(e);
+    })
+});
+
 // console.log("yeet");
 
 const BACKEND_URL = "http://localhost:3000";
@@ -5,72 +19,50 @@ const BACKEND_URL = "http://localhost:3000";
 //     .then(resp => resp.json())
 //     .then(parsedResp => console.log(parsedResp));
 
-document.addEventListener("DOMContentLoaded", () => {
-    const signUpButton = document.getElementById("signUpButton");
+const canvas = document.getElementById("gameScreen");
+const context = canvas.getContext('2d');
+// context.clearRect(0, 0, 800, 600);
+// context.fillStyle = "#00f";
+// // fillRect(x coord, y coord, width, height)
+// context.fillRect(20, 20, 20, 20);
+const GAME_WIDTH = 800; 
+const GAME_HEIGHT = 600;
 
-    const logInButton = document.querySelector("#logInButton");
+const player = new Player(GAME_WIDTH, GAME_HEIGHT);
+const movement = new InputHandler(player);
 
-    signUpButton.addEventListener("click", (e) => {
-        signUp(e);
-    })
+// player.draw(context);
 
-    logInButton.addEventListener("click", (e) => {
-        logIn(e);
-    })
+// let lastTime = 0;
 
-    const canvas = document.getElementById("gameScreen");
-    const context = canvas.getContext('2d');
-    // context.clearRect(0, 0, 800, 600);
-    // context.fillStyle = "#00f";
-    // // fillRect(x coord, y coord, width, height)
-    // context.fillRect(20, 20, 20, 20);
-    const GAME_WIDTH = 800; 
-    const GAME_HEIGHT = 600;
+// gameLoop(context, player);
 
-    const player = new Player(GAME_WIDTH, GAME_HEIGHT);
-    const movement = new InputHandler(player);
-
-    // player.draw(context);
-
-    // let lastTime = 0;
-
-
-
-
-
-
-    // gameLoop(context, player);
-
-});
 
 // function gameLoop(context, player, timestamp, lastTime) {
-
-//     // how much time has passed
-//     let deltaTime = timestamp - lastTime;
-//     lastTime = timestamp;
-
-
-
-//     debugger;
-//     context.clearRect(0, 0, 800, 600);
-//     player.update(deltaTime);
-//     player.draw(context);
-
-
-//     // passes gameLoop() the timestamp
-//     requestAnimationFrame(gameLoop);
-// }
-
+    
+    //     // how much time has passed
+    //     let deltaTime = timestamp - lastTime;
+    //     lastTime = timestamp;
+    
+    //     debugger;
+    //     context.clearRect(0, 0, 800, 600);
+    //     player.update(deltaTime);
+    //     player.draw(context);
+    
+    //     // passes gameLoop() the timestamp
+    //     requestAnimationFrame(gameLoop);
+    // }
+    
 function signUp(e) {
     e.preventDefault();
     console.log("user hit sign up button");
-
+    
     let userInputForUsername = document.querySelector("#usernameForSignUp").value;
-
+    
     let formData = {
         username: userInputForUsername
     }
-
+    
     let configObj = {
         method: "POST", 
         headers: {
@@ -79,7 +71,7 @@ function signUp(e) {
         },
         body: JSON.stringify(formData)
     }
-
+    
     fetch(`${BACKEND_URL}/users`, configObj)
     .then(resp => resp.json())
     .then(parsedResp => {
@@ -87,7 +79,7 @@ function signUp(e) {
         if (parsedResp.username) {
             const currentUser = document.querySelector("#current-user");
             currentUser.innerText = parsedResp.username;
-    
+            
             const highScore = document.querySelector("#levels-completed");
             highScore.innerText = `${parsedResp.levels_completed} levels completed`;
         }
@@ -97,13 +89,13 @@ function signUp(e) {
 function logIn(e) {
     e.preventDefault();
     console.log("user hit log in button");
-
+    
     let userInputForUsername = document.querySelector("#usernameForLogIn").value;
-
+    
     let formData = {
         username: userInputForUsername
     }
-
+    
     let configObj = {
         method: "POST", 
         headers: {
@@ -112,7 +104,7 @@ function logIn(e) {
         },
         body: JSON.stringify(formData)
     }
-
+    
     fetch(`${BACKEND_URL}/login`, configObj)
     .then(resp => resp.json())
     .then(parsedResp => {
@@ -120,9 +112,10 @@ function logIn(e) {
         if (parsedResp.username) {
             const currentUser = document.querySelector("#current-user");
             currentUser.innerText = parsedResp.username;
-    
+            
             const highScore = document.querySelector("#levels-completed");
             highScore.innerText = `${parsedResp.levels_completed} levels completed`;
         }
     });
 }
+
